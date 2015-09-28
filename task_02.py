95b2dae371c93fddc7ce37c1573e7cd17a1fb396
Michael Meyers <mjmeyer2013@gmail.com>
2015-09-27 23:10:18 -0400

    this is all tasks for the warm up.

---
+++ task_02.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This is task 2 the peanut import"""
+
+
+import task_01.peanut
+
+TIME = task_01.peanut.BUTTER
---
+++ task_03.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This is task 3 its jelly time"""
+
+
+from task_01.peanut import BUTTER
+
+JELLY = BUTTER
